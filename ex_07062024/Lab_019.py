e48d701cc6be4c013554e86578213e43c7a1d207
Admin <harshithar4@gmail.com>
2024-06-16 17:49:39 +0530

    Built in functions

---
+++ ex_07062024/Lab_019.py
@@ -0,0 +1,13 @@
+# In built Functions
+name = "Ganishka"
+print(name)
+print(type(name))
+print(id(name))
+print(len(name))
+name1=name.upper()
+print(name1)
+name2=name.lower()
+print(name2)
+print(name.count("a"))
+print(name[0])
+print(name[3])
