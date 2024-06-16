2e04139e6a3a5534541331c7ce3068ba8bdada18
Admin <harshithar4@gmail.com>
2024-06-16 21:32:28 +0530

    Triangle classifier

---
+++ ex_10062024/Lab_027.py
@@ -0,0 +1,6 @@
+a = 1
+print(a)
+v1 = (1 == True)
+v2 = (0 == False)
+print(v1)
+print(v2)
---
+++ ex_14062024/Triangle.py
@@ -0,0 +1,9 @@
+s1 = int(input("Enter the length of first side in cms: "))
+s2 = int(input("Enter the length of second side in cms: "))
+s3 = int(input("Enter the length of third side in cms: "))
+if s1 == s2 == s3:
+    print("All sides are equal, It is a Triangle.")
+elif s1 == s2:
+    print("Two sides are equal,It is a isosceles.")
+else:
+    print("no sides are equal,It is a scalene.")
