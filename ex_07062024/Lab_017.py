d34c7fe3dcd22b423b3f03f91fe1371559688ef4
Admin <harshithar4@gmail.com>
2024-06-16 17:40:05 +0530

    String concatenation & format string

---
+++ ex_07062024/Lab_017.py
@@ -0,0 +1,8 @@
+dir=r"c:\user\app\abc.txt" # to get raw string
+print(dir)
+# formatting string
+first_name="Harry"
+last_name="potter"
+print(first_name +" "+ last_name)
+print(first_name,last_name)
+print(f"My name is {first_name}{last_name}")# formatting the string
