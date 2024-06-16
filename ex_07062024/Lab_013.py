5a6de7e6d444fc0fa19992a30feff47afb216a2f
Admin <harshithar4@gmail.com>
2024-06-16 17:15:47 +0530

    Data types revision exercise

---
+++ ex_07062024/Lab_013.py
@@ -0,0 +1,16 @@
+name="Ganishka"
+age=1
+pi=3.14
+is_female=True
+complex_number=2+3j
+print(type(name))
+print(type(age))
+print(type(pi))
+print(type(is_female))
+print(type(complex_number))
+print(complex_number.real)
+print(complex_number.imag)
+name1=input("Enter your name: ")
+print("My name is ",name1)
+a,b,c=1,2,3
+print(a+b+c)
