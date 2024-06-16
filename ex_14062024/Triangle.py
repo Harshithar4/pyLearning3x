f3653c142d471dec453334af7003073f00bb02fd
Admin <harshithar4@gmail.com>
2024-06-16 23:13:47 +0530

    Task3 triangle

--- ex_14062024/Triangle.py
+++ ex_14062024/Triangle.py
@@ -3,7 +3,7 @@ s2 = int(input("Enter the length of second side in cms: "))
 s3 = int(input("Enter the length of third side in cms: "))
 if s1 == s2 == s3:
     print("All sides are equal, It is a Triangle.")
-elif s1 == s2:
+elif s1 == s2 or s2==s3 or s1==s3:
     print("Two sides are equal,It is a isosceles.")
 else:
     print("no sides are equal,It is a scalene.")
