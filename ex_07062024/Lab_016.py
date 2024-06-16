dcd1fd503b0dc5d477e36ee0d3d523619185e8f7
Admin <harshithar4@gmail.com>
2024-06-16 17:29:36 +0530

    Division operation

---
+++ ex_07062024/Lab_016.py
@@ -0,0 +1,7 @@
+i = 10
+i1 = 20
+print(i / i1) # i/i1 provide floating number 0.5
+i2=20
+i3=10
+print(i2%i3)# It will provide remainder as output 0
+print(i2//i3)# It will provide quotient as output 2
